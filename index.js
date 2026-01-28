1b1f09bb5a4d7378d1a03c02897e2232ba97e364
AnnaCherryLittle <150806066+AnnaCherryLittle@users.noreply.github.com>
2026-01-28 14:30:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+function birthdayCountdown() {
+    const birthdayDate = new Date("January 17, 2027 00:00");
+    const now = new Date();
+    const diff = birthdayDate - now;
+
+    const msInSecond = 1000;
+    const msInMinute = 60 * 1000;
+    const msInHour = 60 * 60 * 1000;
+    const msInDay = 24 * 60 * 60 * 1000;
+
+    const displayDay = Math.floor(diff / msInDay);
+    document.querySelector(".days").textContent = displayDay;
+    const displayHour = Math.floor((diff % msInDay) / msInHour);
+    document.querySelector(".hours").textContent = displayHour;
+    const displayMinute = Math.floor((diff % msInHour) / msInMinute);
+    document.querySelector(".minutes").textContent = displayMinute;
+    const displaySecond = Math.floor((diff % msInMinute) / msInSecond);
+    document.querySelector(".seconds").textContent = displaySecond;
+    if(diff <=0) {
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerID)
+        happyBirthday()
+    }
+}
+
+let timerID = setInterval (birthdayCountdown, 1000);
+
+function happyBirthday() {
+    const heading = document.querySelector("h1");
+    heading.textContent = "З ДНЕМ НАРОДЖЕННЯ!!!";
+    heading.classList.add("red");
+}
+
+const button = document.querySelector("#myButton");
+const audio = document.querySelector("#myAudio");
+
+button.addEventListener("click", function(){
+    if(audio.paused) {
+        audio.play();
+        button.classList.toggle("pause");
+    }
+    else {
+        audio.pause();
+        button.classList.toggle("pause");
+    }
+})
